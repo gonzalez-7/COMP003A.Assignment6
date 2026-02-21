f6bba0c6d1ef7086c064e17c1cd169f1db93d887
gonzalez-7 <20agonzalezp@gmail.com>
2026-02-20 20:33:56 -0800

    Add Book class fields for title, pages, and completion status

---
+++ COMP003A.Assignment6/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.Assignment6
+{
+	internal class Book
+	{
+		//
+		public string Title;
+		public int Pages;
+		public bool Completed;
+	}
+}
