a164b44306a4251344d49a097bb1b98d40132931
gonzalez-7 <20agonzalezp@gmail.com>
2026-02-20 20:36:46 -0800

    Add constructor to initialize Book fields

--- COMP003A.Assignment6/Book.cs
+++ COMP003A.Assignment6/Book.cs
@@ -6,9 +6,17 @@ namespace COMP003A.Assignment6
 {
 	internal class Book
 	{
-		//
+		// store the state of the book
 		public string Title;
 		public int Pages;
 		public bool Completed;
+
+		// Initializes all fields when a new Book object is created
+		public Book(string title, int pages, bool completed)
+		{
+			Title = title;
+			Pages = pages;
+			Completed = completed;
+		}
 	}
 }
